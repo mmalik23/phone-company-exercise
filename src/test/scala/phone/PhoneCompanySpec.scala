d4a71493305c7aedf4316312a05e0eabc7ee1307
Muqeet Malik <muqeetamalik@gmail.com>
2021-05-25 16:15:45 +0100

    Refactor: Use AnyFreeSpec to improve readability

--- src/test/scala/phone/PhoneCompanySpec.scala
+++ src/test/scala/phone/PhoneCompanySpec.scala
@@ -1,4 +1,4 @@
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.freespec.AnyFreeSpec
 import com.phone.PhoneCompany
 import org.scalatest.matchers.should.Matchers
 import java.time.Duration
@@ -6,35 +6,40 @@ import com.phone.Record
 import com.phone.InvalidRecord
 
 
-class PhoneCompanySpec extends AnyFunSuite with Matchers {
+class PhoneCompanySpec extends AnyFreeSpec with Matchers {
     
     val pns = new PhoneCompany
 
-    test("Can parse call logs file") {
-        val read = pns.readFile("calls.log") 
+    "readFile" - {
+        "Can parse call logs file" in {
+
+            val read = pns.readFile("calls.log") 
         
-        read.take(2) shouldBe List(
-            "A 555-333-212 00:02:03",
-            "A 555-433-242 00:06:41"
-        )
-        read.length shouldBe 15
+            read.take(2) shouldBe List(
+                "A 555-333-212 00:02:03",
+                "A 555-433-242 00:06:41"
+            )
+            read.length shouldBe 15
+
+        }
     }
 
-    test("Can parse log record") {
-        pns.parseRawLog("lol rofl 00:02:03") shouldBe Right(Record("lol", "rofl", Duration.ofSeconds(123)))
-    }
+    "parseRawLog" - {
+       "Can parse log record" in {
+            pns.parseRawLog("lol rofl 00:02:03") shouldBe Right(Record("lol", "rofl", Duration.ofSeconds(123)))
+        }
 
-    test("Fail if there are too many rows") {
-        pns.parseRawLog("lol rofl 00:02:03 sd") shouldBe Left(InvalidRecord("Too many space delimited columns"))
-    }
+       "Fail if there are too many rows" in {
+            pns.parseRawLog("lol rofl 00:02:03 sd") shouldBe Left(InvalidRecord("Too many space delimited columns"))
+        }
 
-    test("Fail if there are too few rows") {
-        pns.parseRawLog("lol rofl") shouldBe Left(InvalidRecord("Too few space delimited columns"))
-        pns.parseRawLog("") shouldBe Left(InvalidRecord("Too few space delimited columns"))
-    }
+       "Fail if there are too few rows" in {
+            pns.parseRawLog("lol rofl") shouldBe Left(InvalidRecord("Too few space delimited columns"))
+            pns.parseRawLog("") shouldBe Left(InvalidRecord("Too few space delimited columns"))
+        }
 
-    test("If the time window cannot be parsed") {
-        pns.parseRawLog("lol rofl badduration") shouldBe Left(InvalidRecord("Invalid duration: badduration"))
+       "If the time window cannot be parsed" in {
+            pns.parseRawLog("lol rofl badduration") shouldBe Left(InvalidRecord("Invalid duration: badduration"))
+        }
     }
-
 }
